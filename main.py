f4cfbe6404088f0947d34fdfc5000a6f3a6f7361
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-09-27 19:00:35 +0300

    album project

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import (Flask,
+ render_template,
+ request,
+ make_response,
+ redirect
+ )
+from models import artwork
+
+app = Flask('app')
+artwork.initialize()
+
+
+@app.route('/')
+def index():
+ list_art = artwork.Artwork.select()
+ results = []
+ for art in list_art:
+  art_item = {
+ 'description': art.description,
+ 'thumbnail_link': art.thumbnail_link,
+ 'fullimage_link': art.fullimage_link
+ }
+ results.append(art_item)
+ return render_template("index.html", images=results)
+
+
+app.run(host='0.0.0.0', port=8080)
---
+++ models/artwork.py
@@ -0,0 +1,35 @@
+from peewee import (CharField,
+                    SqliteDatabase,
+                    Model,
+                    TextField,
+                    OperationalError,
+                    IntegrityError)
+db = SqliteDatabase("artwork.db")
+
+
+class Artwork(Model):
+    name = CharField(max_length=1000, unique=True)
+    description = TextField(default="Good image")
+    thumbnail_link = CharField(max_length=1000)
+    fullimage_link = CharField(max_length=1000)
+
+    class Meta:
+        database = db
+
+
+def initialize():
+    try:
+        Artwork.create_table(safe=True)
+    except OperationalError:
+        pass
+    try:
+        Artwork.create(
+            name="monkey_singing",
+            description="Awesome Artistic Monkey",
+            thumbnail_link="static/music-3507317_640.jpg",
+            fullimage_link="static/music-3507317_1920.jpg"
+           
+            
+            )
+    except IntegrityError:
+        pass
